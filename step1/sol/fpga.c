057319c98370b1ba591ad74af8a51fe2e827a032
DiscoverSDK <liran@lirans-MacBook-Pro.local>
2016-09-21 10:25:49 +0300

    first step

--- step1/sol/fpga.c
+++ step1/sol/fpga.c
@@ -15,7 +15,7 @@
 #include <linux/mm.h>
 #include <asm/io.h>
 #include <linux/device.h>
-#include <linux/interrupt.h>    
+#include <linux/interrupt.h>
 
 #define PHY_IO_ADD 0xff007000
 #define PHY_MAP_ADD 0xff008000
@@ -33,7 +33,7 @@
 
 DECLARE_WAIT_QUEUE_HEAD(hq);
 static int flag=0;
- 
+
 static int acme_count = 1;
 static dev_t acme_dev;
 static int *dev_id;
@@ -80,7 +80,7 @@ static int device_ioctl(struct file*file,unsigned int num,
     }
     return 0;
 }
- 
+
 static int device_mmap(struct file *file,struct vm_area_struct* vma)
 {
     int size;
@@ -89,15 +89,15 @@ static int device_mmap(struct file *file,struct vm_area_struct* vma)
         return -EAGAIN;
     return 0;
 }
- 
+
 static struct file_operations acme_fops =
 {
     .owner = THIS_MODULE,
     .unlocked_ioctl = device_ioctl,
     .mmap = device_mmap
 };
- 
- 
+
+
 static irqreturn_t *irq_handle(void * dev_id)
 {
 
@@ -107,27 +107,18 @@ static irqreturn_t *irq_handle(void * dev_id)
     wake_up(&hq);
     return 0;
 }
- 
-extern struct ion_device *idev;
+
 
 static int
 hello_init (void)
 {
-    int *buf;
-    struct ion_handle *data;
     int req_irq = request_irq(6, irq_handle, 0, "myfpga", &dev_id);
     regs = ioremap(PHY_IO_ADD,0x1000);
-
-    client = ion_client_create(idev, -1, "user");
-    data = ion_alloc(client,0x1000,0,ION_HEAP_SYSTEM_MASK);
-    
-    buf = ion_map_kernel(client,data);
-
 
     
     acme_dev = MKDEV(237,0);
     register_chrdev_region(acme_dev,1,"myfpga");
- 
+
     device_create(class_create(THIS_MODULE,"myclass"),NULL,acme_dev,NULL,"myfpga");
     acme_cdev=cdev_alloc();
     if(!acme_cdev)
@@ -137,24 +128,24 @@ hello_init (void)
     }
     acme_cdev->ops = &acme_fops;
     acme_cdev->owner = THIS_MODULE;
- 
+
     if(cdev_add(acme_cdev,acme_dev,acme_count))
     {
         printk (KERN_INFO "cdev add error.\n");
          return -1;
     }
- 
+
   return 0;
- 
+
 }
- 
+
 static void
 hello_cleanup (void)
 {
     cdev_del(acme_cdev);
     unregister_chrdev_region(acme_dev, acme_count);
 }
- 
+
 late_initcall(hello_init);
 module_exit (hello_cleanup);
 MODULE_LICENSE("GPL");
